d790cde798257f82b6d8533db1d8b54a8bcafae9
naufalph <naufal17ph@gmail.com>
2022-11-29 23:14:14 +0700

    seeder and hooks were added on buyer table

--- models/buyer.js
+++ models/buyer.js
@@ -1,7 +1,6 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+const { hashPass } = require("../helpers/bcrypt");
 module.exports = (sequelize, DataTypes) => {
   class Buyer extends Model {
     /**
@@ -11,22 +10,58 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Buyer.hasOne(models.Order)
-      Buyer.hasMany(models.Chat)
+      Buyer.hasOne(models.Order);
+      Buyer.hasMany(models.Chat);
     }
   }
-  Buyer.init({
-    name: DataTypes.STRING,
-    owner: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    phoneNumber: DataTypes.STRING,
-    address: DataTypes.STRING,
-    industry: DataTypes.STRING,
-    website: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Buyer',
-  });
+  Buyer.init(
+    {
+      name: DataTypes.STRING,
+      owner: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          msg: "Email must be unique",
+        },
+        validate: {
+          isEmail: {
+            msg: "Invalid email format",
+          },
+          notNull: {
+            msg: "Email is required",
+          },
+          notEmpty: {
+            msg: "Email is required",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Password is required",
+          },
+          notEmpty: {
+            msg: "Password is required",
+          },
+        },
+      },
+      phoneNumber: DataTypes.STRING,
+      address: DataTypes.STRING,
+      industry: DataTypes.STRING,
+      website: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Buyer",
+      hooks:{
+        beforeCreate:(instance)=>{
+          instance.password = hashPass(instance.password);
+        }
+      }
+    }
+  );
   return Buyer;
 };
