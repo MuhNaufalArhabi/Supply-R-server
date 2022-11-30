7fe2149dd0aa132d24c550ca51ea4c984baaa221
naufalph <naufal17ph@gmail.com>
2022-11-30 20:22:55 +0700

    added authentication

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -5,6 +5,12 @@ const errorHandler = async (error, req, res, next) => {
   if (error.name === "NotFound") {
     code = 400;
     message = "Buyer not Found";
+  } else if (
+    error.name === "invalidToken" ||
+    error.name === `JsonWebTokenError`
+  ) {
+    code = 401;
+    message = "authentication failed";
   }
   console.log(error);
   res.status(code).json({ message });
--- routes/buyerRoute.js
+++ routes/buyerRoute.js
@@ -1,10 +1,14 @@
 const ControllerJalurAtas = require("../controllers/controllerJalurAtas");
+const {authenticateBuyer} = require("../middlewares/authenAtas");
 
 const router = require("express").Router();
 
-router.get("/", ControllerJalurAtas.getBuyers);
-router.post("/", ControllerJalurAtas.postBuyer);
 router.post("/login", ControllerJalurAtas.buyerLogin);
+router.post("/register", ControllerJalurAtas.postBuyer);
+
+// router.use(authenticateBuyer);
+
+router.get("/", ControllerJalurAtas.getBuyers);
 router.get("/:id", ControllerJalurAtas.getOneBuyer);
 router.delete("/:id", ControllerJalurAtas.delBuyer);
 
