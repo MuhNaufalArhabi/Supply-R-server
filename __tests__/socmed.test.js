b47c18caf9d55082194be33aa418251e77743295
irwNd2 <irwandduadua@gmail.com>
2022-12-05 18:43:11 +0800

    jf

--- __tests__/socmed.test.js
+++ __tests__/socmed.test.js
@@ -1,7 +1,6 @@
 const request = require("supertest");
 const app = require("../app");
 const { Seller } = require("../models");
-const { encode } = require("../helpers/jwt");
 
 
 afterAll((done) => {
