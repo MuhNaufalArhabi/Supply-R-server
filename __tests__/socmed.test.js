f24d78df64860950b602f17d6f3682ce079e784b
irwNd2 <irwandduadua@gmail.com>
2022-12-05 18:41:37 +0800

    Baru 1

---
+++ __tests__/socmed.test.js
@@ -0,0 +1,39 @@
+const request = require("supertest");
+const app = require("../app");
+const { Seller } = require("../models");
+const { encode } = require("../helpers/jwt");
+
+
+afterAll((done) => {
+  Seller.destroy({ where: {} })
+    .then(() => {
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
+
+describe("POST /login", () => {
+  describe("Success Login", () => {
+    test("should send an object (access_token) with 200 status code", (done) => {
+      request(app)
+        .post("/sellers/google-login")
+        .send({
+            email: "test.google@mail.com",
+            username: "test google",
+        })
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          } else {
+            expect(res.status).toBe(200);
+            expect(res.body).toBeInstanceOf(Object);
+            expect(res.body).toHaveProperty("access_token", expect.any(String));
+            expect(res.body).toHaveProperty("message", "login Google ok");
+            done();
+          }
+        });
+    });
+  });
+});
